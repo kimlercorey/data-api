f8d885c3d10d1c7f31bac3da43f516c31ca4b11e
Kimler (KC) Corey <kimler@gmail.com>
2013-06-18 06:09:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataApi::Application.config.secret_token = 'b242bd604f8894ee8647497b6d2806d47027747bd2e63f9fa477464ceb67adca027e3bba48d942558c0f4eab3ee19a254135ee5bd81c713621e491f410943821'
