2c7e4299c50b869bc2ca7528de386c3de54f638e
Kimler (KC) Corey <kimler@gmail.com>
2013-06-21 21:14:19 -0400

    modified initializer

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -1,5 +1,6 @@
 # config/initializers/her.rb
-Her::API.setup url: "http://datafly.data-api.com" do |c|
+
+Her::API.setup url:'http://data-api.com' do |c|
       c.use Faraday::Request::UrlEncoded
       c.use Her::Middleware::DefaultParseJSON
       c.use Faraday::Adapter::NetHttp
